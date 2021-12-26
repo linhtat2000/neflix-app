dfefe9232ffd2150a5ec40761bf243c76226e2fe
xiaoling2000 <linh2000business@gmail.com>
2021-12-26 21:28:10 +0700

    done UI of the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      {/* <Navbar /> */}
+      {/* <Home /> */}
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 }
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -4,21 +4,44 @@ import {
   ThumbDownAltOutlined,
   ThumbUpAltOutlined,
 } from "@material-ui/icons";
+import { useState } from "react";
 import "./listItem.scss";
 
-const ListItem = () => {
+const ListItem = ({ index }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const trailer = "";
+
   return (
-    <div className="listItem">
+    <div
+      className="listItem"
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <img src="" alt="movie thumbnail" />
-      <div className="itemInfo">
-        <div className="icons">
-          <PlayArrow />
-          <Add />
-          <ThumbUpAltOutlined />
-          <ThumbDownAltOutlined />
-        </div>
-        <div className="itemInfoTop"></div>
-      </div>
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow className="icon" />
+              <Add className="icon" />
+              <ThumbUpAltOutlined className="icon" />
+              <ThumbDownAltOutlined className="icon" />
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 15 mins</span>
+              <span className="limit">+15</span>
+              <span>1999</span>
+            </div>
+            <div className="desc">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. A
+              ducimus tempora praesentium ipsam iste?
+            </div>
+            <div className="genre">Action</div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
